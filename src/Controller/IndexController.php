f066e81992e8d17338ad85c3786e82e756ae791c
m.franosz <mfranosz15@gmail.com>
2023-03-08 23:56:24 +0100

    Creating connection with api

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\Client;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
@@ -17,8 +21,15 @@ class IndexController extends AbstractController
     }
 
     #[Route('/add', name: 'add_new', methods: 'GET')]
-    public function add()
+    public function add(Request $request) : JsonResponse
     {
+        if ($request->isMethod('GET')) {
+            $client = new Client;
 
+            $request = $client->request('GET', 'http://api.nbp.pl/api/exchangerates/tables/a/?format=json');
+
+            echo $request->getBody();
+
+        }
     }
 }
