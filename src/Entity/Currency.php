22ed01823f3583e44ba50cff9b11c1e93230718f
m.franosz <mfranosz15@gmail.com>
2023-03-08 23:05:09 +0100

    Create field in Currency Entity

--- src/Entity/Currency.php
+++ src/Entity/Currency.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CurrencyRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CurrencyRepository::class)]
@@ -13,8 +14,68 @@ class Currency
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $currency_code = null;
+
+    #[ORM\Column]
+    private ?float $exchange_rate = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $uploaded_at = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCurrencyCode(): ?string
+    {
+        return $this->currency_code;
+    }
+
+    public function setCurrencyCode(string $currency_code): self
+    {
+        $this->currency_code = $currency_code;
+
+        return $this;
+    }
+
+    public function getExchangeRate(): ?float
+    {
+        return $this->exchange_rate;
+    }
+
+    public function setExchangeRate(float $exchange_rate): self
+    {
+        $this->exchange_rate = $exchange_rate;
+
+        return $this;
+    }
+
+    public function getUploadedAt(): ?\DateTimeInterface
+    {
+        return $this->uploaded_at;
+    }
+
+    public function setUploadedAt(\DateTimeInterface $uploaded_at): self
+    {
+        $this->uploaded_at = $uploaded_at;
+
+        return $this;
     }
 }
