2b3c6f764c0a31befbe76780884fc35020b3a144
m.franosz <mfranosz15@gmail.com>
2023-03-08 23:27:32 +0100

    Create IndexController

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/add', name: 'add_new', methods: 'GET')]
+    public function add()
+    {
+
+    }
+}
